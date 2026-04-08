a55251204aa73df51447cfb85763a0c810e4fa32
UPEV1sion <e.schaeffer82@gmail.com>
2026-04-08 18:05:45 +0200

    implement dlx

--- dlx.c
+++ dlx.c
@@ -4,102 +4,214 @@
 
 #define shift_args(argc, argv) ((argc)--, *(argv)++)
 
-typedef struct Node Node;
 typedef struct Column Column;
+typedef struct Node Node;
 
 struct Node {
-    Node *l, *r, *d, *u;
+    Node *l, *r, *u, *d;
     Column *c;
 };
 
 struct Column {
     Node header;
-    char *name;
-    size_t count;
+    size_t s;
+    size_t n;
 };
 
-Column* parse_matrix(const char *path, size_t *ncols, size_t *nrows, size_t *npieces)
+Node* new_node(Column *c)
+{
+    Node *n = malloc(sizeof(Node));
+    assert(n);
+    n->l = n->r = n->u = n->d = n;
+    n->c = c;
+    return n;
+}
+
+void columns_add_node(Column *c, Node *n)
+{
+    Node *header = &c->header;
+    n->u = header->u;
+    n->d = header;
+    header->u->d = n;
+    header->u = n;
+    n->c = c;
+    c->s++;
+}
+
+void link_row(Node **row_nodes, const size_t count)
+{
+    for(size_t i = 0; i < count; ++i)
+    {
+        row_nodes[i]->r = row_nodes[(i + 1) % count];
+        row_nodes[i]->l = row_nodes[(i + count - 1) % count];
+    }
+}
+
+void cover_column(Column *c)
+{
+    c->header.r->l = c->header.l;
+    c->header.l->r = c->header.r;
+    const Node *start = &c->header;
+    for(Node *i = start->d; i != start; i = i->d)
+    {
+        for(Node *j = i->r; j != i; j = j->r)
+        {
+            j->d->u = j->u;
+            j->u->d = j->d;
+            j->c->s--;
+        }
+    }
+}
+
+void uncover_column(Column *c)
+{
+    const Node *start = &c->header;
+    for(Node *i = start->u; i != start; i = i->u)
+    {
+        for(Node *j = i->l; j != i; j = j->l)
+        {
+            j->c->s++;
+            j->d->u = j;
+            j->u->d = j;
+        }
+    }
+    
+    c->header.r->l = &c->header;
+    c->header.l->r = &c->header;
+}
+
+Node* parse_matrix_from_file(const char *path, size_t *nrows, size_t *ncols, size_t *npieces)
 {
     FILE *f;
-    assert((f = fopen(path, "r")));
+    assert((f = fopen(path, "r")) != NULL);
 
-    fscanf(f, "%zu", ncols);
     fscanf(f, "%zu", nrows);
+    fscanf(f, "%zu", ncols);
     fscanf(f, "%zu", npieces);
-
     Column *columns = malloc(*ncols * sizeof(Column));
     assert(columns);
+
+    Node *root = malloc(sizeof(Node));
+    assert(root);
+
+    root->l = root->r = root;
+    root->u = root->d = root;
+    root->c = NULL;
     
     for(size_t i = 0; i < *ncols; ++i)
     {
-        columns[i].name = NULL;
-        columns[i].count = 0;
+        columns[i].s = 0;
+        columns[i].n= i;
         columns[i].header.c = &columns[i];
-        columns[i].header.d = &columns[i].header;
         columns[i].header.u = &columns[i].header;
-    } 
-    
-    Node root;
-    root.r = root.l = &root;
+        columns[i].header.d = &columns[i].header;
+        columns[i].header.l = root;
+        columns[i].header.r = root->r;
 
-    for(size_t i = 0; i < *ncols; ++i)
-    {
-        Node *col_node = &columns[i].header;
-
-        col_node->r = root.r;
-        col_node->l = &root;
-        root.r->l = col_node;
-        root.r = col_node;
+        root->r->l = &columns[i].header;
+        root->r = &columns[i].header;
     }
-    while (1)
+
+    char line[1024];
+    while(fgets(line, sizeof line, f))
     {
-        size_t col;
+        size_t count = 0;
+        size_t positions[128];
 
-        if (fscanf(f, "%zu", &col) != 1)
-            break;
-
-        Node *first = NULL;
-        Node *prev = NULL;
-
-        do
+        size_t idx;
+        char *ptr = line;
+        while(sscanf(ptr, "%zu", &idx) == 1)
         {
-            Node *node = malloc(sizeof(Node));
+            positions[count++] = idx;
 
-            node->c = &columns[col];
+            
+            while(*ptr && *ptr != ' ') ptr++;
+            while(*ptr == ' ') ptr++;
+        }
+        
+        if(count == 0) continue;
 
-            node->d = &columns[col].header;
-            node->u = columns[col].header.u;
-            columns[col].header.u->d = node;
-            columns[col].header.u = node;
-            columns[col].count++;
+        Node **row_nodes = malloc(count * sizeof(Node*));
+        assert(row_nodes);
 
-            if (!first)
-            {
-                first = node;
-                node->l = node->r = node;
-            }
-            else
-            {
-                node->l = prev;
-                node->r = first;
-                prev->r = node;
-                first->l = node;
-            }
+        for(size_t i = 0; i < count; ++i)
+        {
+            Column *col = columns + positions[i];
+            Node *n = new_node(col);
+            columns_add_node(col, n);
+            row_nodes[i] = n;
+        }
 
-            prev = node;
-
-            const int c = fgetc(f);
-            if (c == '\n' || c == EOF)
-                break;
-
-            ungetc(c, f);
-
-        } while (fscanf(f, "%zu", &col) == 1);
+        link_row(row_nodes, count);
+        free(row_nodes);
     }
 
     fclose(f);
 
-    return columns;
+    return root;
+}
+
+Node* choose_column(Node *root)
+{
+    Node *best = root->r;
+
+    for(Node *c = root->r; c != root; c = c->r)
+    {
+        if(c->c->s < best->c->s) best = c;
+    }
+
+    return best;
+}
+
+void print_solution(Node **o, const size_t k)
+{
+    for(size_t i = 0; i < k; ++i)
+    {
+        Node *row = o[i];
+
+        Node *j = row;
+        do
+        {
+            printf("%zu ", j->c->n);
+            j = j->r;
+        } while(j != row);
+
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
+void search(Node *root, Node **o, size_t k)
+{
+    if(root == root->r) 
+    {
+        print_solution(o, k);
+        return;
+    }
+    
+    Node *c_node = choose_column(root);
+    Column *c = c_node->c;
+    cover_column(c);
+
+    for(Node *r = c->header.d; r != &c->header; r = r->d)
+    {
+        o[k] = r;
+
+        for(Node *j = r->r; j != r; j = j->r)
+        {
+            cover_column(j->c);
+        }
+
+        search(root, o, k + 1);
+
+        for(Node *j = r->l; j != r; j = j->l)
+        {
+            uncover_column(j->c);
+        }
+    }
+
+    uncover_column(c);
 }
 
 int main(int argc, char **argv)
@@ -112,9 +224,13 @@ int main(int argc, char **argv)
     }
 
     const char *path = shift_args(argc, argv);
-    size_t ncols, nrows, npieces;
-    Column *column = parse_matrix(path, &ncols, &nrows, &npieces);
-
+    size_t nrows;
+    size_t ncols;
+    size_t npieces;
+    Node *matrix = parse_matrix_from_file(path, &nrows, &ncols, &npieces);
+    Node **o = malloc(sizeof(Node*) * ncols);
+    assert(o);
+    search(matrix, o, 0);
 
     return 0;
 }
